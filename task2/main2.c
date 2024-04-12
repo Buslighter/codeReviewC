d1341dd46ec25042df26f3e54459d5f47157471c
Zoraida Daphine <zoraidad@ga-e5.msk.21-school.ru>
2024-04-12 19:55:51 +0300

    task-2

--- task2/main2.c
+++ task2/main2.c
@@ -1,67 +1,17 @@
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int fillMatrix(int** matrix, int size) {
-  int ans = 0;
-  for (int i = 0; i < size; i++) {
-    matrix[i] = (int*)malloc(size * sizeof(int));
-    for (int j = 0; j < size; j++) {
-      if (scanf("%d", &matrix[i][j]) != 1) {
-        ans = 1;
-        break;
-      }
-    }
-  }
-  if (ans) {
-    for (int i = 0; i < size; i++) {
-      free(matrix[i]);
-    }
-    free(matrix);
-  }
-  return ans;
-}
-void freeMatrix(int** matrix, int size) {
-  for (int i = 0; i < size; i++) {
-    free(matrix[i]);
-  }
-  free(matrix);
-}
-
 int main() {
-  int n;
+  int r;
   int ans = 0;
-  if (scanf("%d\n", &n) != 1) {
+  if (scanf("%d", &r) != 1) {
     ans = 1;
   }
   if (ans != 1) {
-    int** A = (int**)malloc(n * sizeof(int*));
-    int** B = (int**)malloc(n * sizeof(int*));
-    int** result = (int**)malloc(n * sizeof(int*));
-    ans = fillMatrix(A, n);
-    ans = fillMatrix(B, n);
-    if (ans != 1) {
-      for (int i = 0; i < n; i++) {
-        result[i] = (int*)malloc(n * sizeof(int));
-        for (int j = 0; j < n; j++) {
-          int sum = 0;
-          for (int k = 0; k < n; k++) {
-            sum += A[i][k] * B[k][j];
-          }
-          result[i][j] = sum;
-        }
-      }
-      for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-          printf("%d ", result[i][j]);
-        }
-        printf("\n");
-      }
-      freeMatrix(result, n);
-      freeMatrix(A, n);
-      freeMatrix(B, n);
-    } else {
-      printf("n/a");
-    }
+    double V = (4. / 3.) * M_PI * pow(r, 3);
+    double p = 21500.;
+    printf("%0.lf", V * p);
   } else
     printf("n/a");
   return ans;
